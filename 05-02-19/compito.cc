bb8b12af974e4c71c58eb59175b0302301182c5d
simoare732 <simone.aresta03@gmail.com>
2023-07-06 10:06:05 +0200

    Finished point 1

--- 05-02-19/compito.cc
+++ 05-02-19/compito.cc
@@ -1,6 +1,7 @@
 #include "tipo.h"
 #include "liste.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -12,10 +13,37 @@ void pesca(lista &l){
     l = ord_insert_elem(l,e);
 }
 
+void pescaFile(lista &l, int n, char file[]){
+    ifstream f(file);
+    tipo_inf nuovo;
+    for(int i=0;i<n;i++){
+        f>>nuovo.valore>>nuovo.seme;
+        lista e = new_elem(nuovo);
+        l = ord_insert_elem(l,e);
+    }
+}
+
 void stampa(lista l){
     while(l != NULL){
         print(head(l));
         cout<<" ";
         l = tail(l);
     }
+}
+
+int main(){
+    lista g1 = NULL, g2 = NULL;
+    /*cout<<"Carte per il giocatore 1: ";
+    for(int i=0;i<6;i++)
+        pesca(g1);
+    cout<<endl<<"Carte per il giocatore 2: ";
+    for(int i=0;i<6;i++)
+        pesca(g2);*/
+    pescaFile(g1, 6, "card1.txt");
+    pescaFile(g2, 6, "card2.txt");
+    cout<<"Giocatore 1: ";
+    stampa(g1);
+    cout<<endl<<"Giocatore 2: ";
+    stampa(g2);
+    cout<<endl;
 }
