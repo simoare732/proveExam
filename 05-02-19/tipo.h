35cb035ea36893b5256201022c99bc686fe768c3
Simone Aresta <63786933+simoare732@users.noreply.github.com>
2023-07-06 09:27:10 +0200

    Add files via upload

---
+++ 05-02-19/tipo.h
@@ -0,0 +1,14 @@
+/*******************************/
+/* HEADER MODULO "tipo" */
+/*******************************/
+
+
+
+typedef struct carta{
+	char seme;
+	int valore;
+};
+
+int compare(tipo_inf,tipo_inf);
+void copy(tipo_inf&,tipo_inf);
+void print(tipo_inf);
