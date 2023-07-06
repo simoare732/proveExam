2ead38bd40a30ce5f20385def6ebd8ad4b87fe68
simoare732 <simone.aresta03@gmail.com>
2023-07-06 09:42:55 +0200

    create pesca e stampa

--- 05-02-19/compito.cc
+++ 05-02-19/compito.cc
@@ -4,3 +4,18 @@
 
 using namespace std;
 
+void pesca(lista &l){
+    tipo_inf nuovo;
+    cout<<"Inserisci una nuova carta valore seme: ";
+    cin>>nuovo.valore>>nuovo.seme;
+    lista e = new_elem(nuovo);
+    l = ord_insert_elem(l,e);
+}
+
+void stampa(lista l){
+    while(l != NULL){
+        print(head(l));
+        cout<<" ";
+        l = tail(l);
+    }
+}
